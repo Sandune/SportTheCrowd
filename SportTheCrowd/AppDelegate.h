7d1c77461168a512a7fa819c22d857f9c531d846
黄景川 <hjcshaqiu@163.com>
2016-05-23 14:41:08 +0800

    first

---
+++ SportTheCrowd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SportTheCrowd
+//
+//  Created by 黄景川 on 16/5/23.
+//  Copyright © 2016年 huangjingchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SportTheCrowd/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SportTheCrowd
+//
+//  Created by 黄景川 on 16/5/23.
+//  Copyright © 2016年 huangjingchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
