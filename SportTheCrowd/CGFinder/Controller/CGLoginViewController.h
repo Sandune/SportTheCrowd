f266388c1229a5d263570e7ff2f5471c0c7cfbd0
黄景川 <hjcshaqiu@163.com>
2016-05-24 09:12:52 +0800

    JC

---
+++ SportTheCrowd/CGFinder/Controller/CGLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  CGLoginViewController.h
+//  SportTheCrowd
+//
+//  Created by 黄景川 on 16/5/23.
+//  Copyright © 2016年 huangjingchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CGLoginViewController : UIViewController
+
+@end
---
+++ SportTheCrowd/CGFinder/Controller/CGLoginViewController.m
@@ -0,0 +1,43 @@
+//
+//  CGLoginViewController.m
+//  SportTheCrowd
+//
+//  Created by 黄景川 on 16/5/23.
+//  Copyright © 2016年 huangjingchuan. All rights reserved.
+//
+
+#import "CGLoginViewController.h"
+
+
+@interface CGLoginViewController ()
+
+@end
+
+@implementation CGLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(UIViewController *)childViewControllerForStatusBarHidden{
+    return nil;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
